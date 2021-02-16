fee7bcc8112f5e26e56840f01fbbbbdf8913381c
leonardobussi <leonardobezerrabussy@gmail.com>
2021-02-16 11:26:28 -0300

    despareado

--- hooks/src/views/examples/Home.jsx
+++ hooks/src/views/examples/Home.jsx
@@ -3,6 +3,7 @@ import PageTitle from '../../components/layout/PageTitle'
 
 const Home = props => (
     <div className="Home">
+        
         <PageTitle
             title="Módulo Hooks"
             subtitle="Hooks são uma nova adição ao React 16.8." />
